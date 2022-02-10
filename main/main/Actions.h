7c72d77b220324bfa7362c7837f66f829ff92963
dani dimov <gkhrisimov19@codingburgas.bg>
2022-02-10 21:41:59 +0200

    Add Actions.h

---
+++ main/main/Actions.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+struct Article {
+	string nameOfArticle;
+	string data;
+	string participants;
+	int yearOfEvent;
+	int ID;  //Код на Грей
+	
+};
--- main/main/main.cpp
+++ main/main/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-#include "Menu.h"
 
 using namespace std;
 
-int main()
+
+void removeSpecialNode(int n)
 {
-    options();
+    cout << 11 << " " << 12 << " " << 13;
 }
 
+int main()
+{
+    int n;
+    cin >> n;
+    cout << 11 << " " << n << " " << 12 << " " << 13 << endl;
+    removeSpecialNode(n);
+}

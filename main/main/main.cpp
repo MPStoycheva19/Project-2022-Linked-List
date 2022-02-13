792d2bce96f266c1f3c8c3727b8bb205dba8c192
dani dimov <gkhrisimov19@codingburgas.bg>
2022-02-13 21:48:46 +0200

    Add comments

--- main/main/main.cpp
+++ main/main/main.cpp
@@ -7,6 +7,6 @@ using namespace std;
 int main()
 {
     HWND hWnd = GetConsoleWindow();
-    ShowWindow(hWnd, SW_SHOWMAXIMIZED);
+    ShowWindow(hWnd, SW_SHOWMAXIMIZED); //Open the console in full screen
     options();
 }
